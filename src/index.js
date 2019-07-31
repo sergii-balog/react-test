cc32505fa890e386c22f59dc88112068c0306c12
sbalog <sbalog@lanteria.net>
2019-07-31 15:48:41 +0300

    index changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.min.css";
-import Cart from "./components/cart/cart";
+//import Cart from "./components/cart/cart";
 import MoviesList from "./components/movies/moviesList";
 
-ReactDOM.render(
-  <Cart dataUrl="http://localhost:4546/api/cart" />,
-  document.getElementById("root")
-);
-//ReactDOM.render(<MoviesList />, document.getElementById("root"));
+// ReactDOM.render(
+//   <Cart dataUrl="http://localhost:4546/api/cart" />,
+//   document.getElementById("root")
+// );
+ReactDOM.render(<MoviesList />, document.getElementById("root"));
