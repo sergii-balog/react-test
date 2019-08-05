3eb68dd3007ec0cce5187f23b43c6f2d3e7e808e
sergii.balog <sbal323@gmail.com>
2019-08-05 23:57:29 +0300

    routers added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import Movies from "./movies/movies";
+import { Route, Switch } from "react-router-dom";
+import Products from "./products";
+import NavBar from "./navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="container p-2">
-        <div>Navigation</div>
-        <Movies />
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/products" component={Products} />
+            <Route path="/" exact component={Movies} />
+          </Switch>
+        </div>
       </div>
     );
   }
