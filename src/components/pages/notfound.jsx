184dc8fae6a031223101b34db63729103f537d57
sbalog <sbalog@lanteria.net>
2019-08-07 20:01:28 +0300

    class fix

--- src/components/pages/notfound.jsx
+++ src/components/pages/notfound.jsx
@@ -4,13 +4,13 @@ const NotFound = () => {
   return (
     <React.Fragment>
       <br />
-      <div class="row">
-        <div class="col-sm-6">
-          <div class="card">
-            <div class="card-header">Page not found</div>
-            <div class="card-body">
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="card">
+            <div className="card-header">Page not found</div>
+            <div className="card-body">
               <h5 className="text-warning">
-                <i class="fa fa-exclamation-triangle p-2" />
+                <i className="fa fa-exclamation-triangle p-2" />
                 <span className="text-dark">
                   Oops... we can not find the page requested.
                 </span>
